9d7ee24a8ea0b4d5bedf4795195c976b7ce39558
muhifni <muhammad.gcs@gmail.com>
2021-06-01 23:50:06 +0800

    fix some bugs

--- src/server.js
+++ src/server.js
@@ -7,7 +7,8 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: '54.255.162.115',
+    // eslint-disable-next-line max-len
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '54.255.162.115',
     routes: {
       cors: {
         origin: ['*'],
@@ -21,5 +22,5 @@ const init = async () => {
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
 
-
+console.log(`Mode environment ${process.env.NODE_ENV}`);
 init();

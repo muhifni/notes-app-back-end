9718e6d092c419143491266809cca6527ec026ce
muhifni <muhammad.gcs@gmail.com>
2021-06-04 05:12:33 +0800

    mengganti port server ke 5000

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 5500,
+    port: 5000,
     // eslint-disable-next-line max-len
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.39.242',
     routes: {
